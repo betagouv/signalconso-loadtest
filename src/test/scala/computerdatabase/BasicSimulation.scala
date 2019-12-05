546e10fc617158ed3c20257287b555c6e5f102af
Thomas Chaumeny <thomas.chaumeny@signalconso.beta.gouv.fr>
2019-12-05 15:19:28 +0100

    First working version posting reports

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -3,50 +3,50 @@ package computerdatabase
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
+import scala.util.Random
 
 class BasicSimulation extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("http://computer-database.gatling.io") // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .baseUrl("https://app-513c11a7-052f-4ff1-8670-5a17dacb7327.cleverapps.io")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("request_1")
-      .get("/"))
-    .pause(7) // Note that Gatling has recorder real time pauses
-    .exec(http("request_2")
-      .get("/computers?f=macbook"))
-    .pause(2)
-    .exec(http("request_3")
-      .get("/computers/6"))
-    .pause(3)
-    .exec(http("request_4")
-      .get("/"))
-    .pause(2)
-    .exec(http("request_5")
-      .get("/computers?p=1"))
-    .pause(670 milliseconds)
-    .exec(http("request_6")
-      .get("/computers?p=2"))
-    .pause(629 milliseconds)
-    .exec(http("request_7")
-      .get("/computers?p=3"))
-    .pause(734 milliseconds)
-    .exec(http("request_8")
-      .get("/computers?p=4"))
-    .pause(5)
-    .exec(http("request_9")
-      .get("/computers/new"))
-    .pause(1)
-    .exec(http("request_10") // Here's an example of a POST request
-      .post("/computers")
-      .formParam("""name""", """Beautiful Computer""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
-      .formParam("""introduced""", """2012-05-30""")
-      .formParam("""discontinued""", """""")
-      .formParam("""company""", """37"""))
+  val firstNames = Array("John", "Jane", "Margaret", "Georges", "Kate")
 
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
+  val randomFeeder = Iterator.continually(Map(
+    "firstName" -> firstNames(Random.nextInt(firstNames.length)),
+    "siret"     -> Random.alphanumeric.filter(_.isDigit).take(9).mkString,
+    "number"    -> Random.nextInt(1000000)
+  ))
+
+  val scn = scenario("Submit a report without attachment")
+    .feed(randomFeeder)
+    .exec(http("Send report")
+      .post("/api/reports")
+      .body(StringBody("""
+      {
+        "category":"Café / Restaurant",
+        "subcategories":["Hygiène","J'ai vu un animal (nuisible)"],
+        "companyName":"OLA PIZZ",
+        "companyAddress":"OLA PIZZ - 7 B RUE DES COURANCES - 37500 CHINON",
+        "companyPostalCode":"37500",
+        "companySiret":"83493288100016",
+        "firstName":"${firstName}",
+        "lastName":"Doe${number}",
+        "email":"doe${number}@example.com",
+        "contactAgreement":false,
+        "employeeConsumer":false,
+        "files":[],
+        "details":[
+          {"label":"Date du constat :","value":"05/12/2019"},
+          {"label":"Quel animal avez-vous vu&#160;:","value":"Rongeurs (rat, souris...)"},
+          {"label":"Où l'avez-vous vu&#160;:","value":"Dans mon plat"}
+        ]}
+      """)).asJson
+    )
+
+  setUp(scn.inject(atOnceUsers(2)).protocols(httpProtocol))
 }
